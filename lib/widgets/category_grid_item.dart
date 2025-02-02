70cbf51b0ae6c00c1f97b6c1dba025a97d742e9a
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-02 18:06:38 +0100

    Added this file to disply a grid, which is all the categories

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+import 'package:meal_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withValues(alpha: 0.55),
+            category.color.withValues(alpha: 0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+      ),
+    );
+  }
+}
