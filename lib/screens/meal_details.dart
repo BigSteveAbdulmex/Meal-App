eb1c15458f9f10cd1ac7df917f6d5cd7bf67a5ce
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-03 22:11:31 +0100

    Created the meal details screen

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:meal_app/models/meal.dart';
+
+class MealDetails extends StatelessWidget {
+  const MealDetails({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
