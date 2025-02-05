86e8386e4e89bbc2d7012c6fecf01ca414778f37
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-05 21:01:09 +0100

    Added favourites functionality

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -3,15 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
 
 class MealDetails extends StatelessWidget {
-  const MealDetails({super.key, required this.meal});
+  const MealDetails({
+    super.key,
+    required this.meal,
+    required this.onToggleFavourite,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavourite;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavourite(meal);
+            },
+            icon: Icon(Icons.star),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
