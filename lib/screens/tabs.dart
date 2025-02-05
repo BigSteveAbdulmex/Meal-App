8e0141a7859c0c950f2bc4c8bb9dea2e64ce033e
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-05 20:13:22 +0100

    Created bottom navigation barr

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/screens/categories.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() {
+    return _TabsState();
+  }
+}
+
+class _TabsState extends State<Tabs> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dynamic'),
+      ),
+      body: const Placeholder(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+        ],
+      ),
+    );
+  }
+}
