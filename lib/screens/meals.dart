b1ac1252fb70aee60ab2ba963845e3b74bf44f03
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-05 21:01:40 +0100

    Added favourites functionality

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -8,15 +8,19 @@ class MealsScreen extends StatelessWidget {
     super.key,
     this.title,
     required this.meals,
+    required this.onToggleFavourite,
   });
 
   final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavourite;
+
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealDetails(
           meal: meal,
+          onToggleFavourite: onToggleFavourite,
         ),
       ),
     );
