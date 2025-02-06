873a4fef221ce59d1015ef72b31cfc6fc9918f08
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-06 22:59:08 +0100

    Update the code for the favourite feature

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meal_app/providers/meals_provider.dart';
 import 'package:meal_app/providers/favourites_provider.dart';
+import 'package:meal_app/providers/filters_provider.dart';
 
 import 'package:meal_app/screens/categories.dart';
 import 'package:meal_app/screens/filters.dart';
