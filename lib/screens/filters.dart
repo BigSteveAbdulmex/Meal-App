f0df36681cf49827557acc8c3da1cd58703d1047
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-06 22:58:51 +0100

    Update the code for the favourite feature

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meal_app/providers/filters_provider.dart';
 // import 'package:meal_app/screens/tabs.dart';
 
 // import 'package:meal_app/widgets/main_drawer.dart';
 
-enum Filter {
-  glutenFree,
-  lactoseFree,
-  vegetarian,
-  vegan,
-}
-
-class FiltersScreen extends StatefulWidget {
+class FiltersScreen extends ConsumerStatefulWidget {
   const FiltersScreen({
     super.key,
     required this.currentFilters,
@@ -19,12 +16,12 @@ class FiltersScreen extends StatefulWidget {
   final Map<Filter, bool> currentFilters;
 
   @override
-  State<FiltersScreen> createState() {
+  ConsumerState<FiltersScreen> createState() {
     return _FiltersScreenState();
   }
 }
 
-class _FiltersScreenState extends State<FiltersScreen> {
+class _FiltersScreenState extends ConsumerState<FiltersScreen> {
   var _glutenFreeFilterSet = false;
   var _lactoseFreeFilterSet = false;
   var _vegetarianFilterSet = false;
